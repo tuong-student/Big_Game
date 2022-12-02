0428a780102b390154b6f726966ab90052184728
Tuong <tuongnt@vitalify.asia>
2022-12-02 17:23:00 +0700

    Complete go to manager

--- Big_game/Assets/Scripts/Common/Main.cs
+++ Big_game/Assets/Scripts/Common/Main.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
-public class Main : MonoBehaviour
+public class Main : MonoBehaviorInstance<Main>
 {
     public Transform respawnPos;
     PlayerScripts player;
 
-    private void Start()
-    {
-        player = PlayerScripts.Create(respawnPos);
-        
-        GameCanvas.Create();
 
-        GameManager.Create();
-        UIManager.Create();
+    private IEnumerator Start()
+    {
+        LevelManager.Create();
         GoldManager.Create();
         PoolingManager.Create();
+
+        GameCanvas.Create().AddTo(this);
+        GameManager.Create().AddTo(this);
+        UIManager.Create().AddTo(this);
+
+        Debug.Log(LocalDataManager.currentLevel);
+        LevelManager.OnNextLevel += GenerateNewLevel;
+
+        yield return null;
+        if (respawnPos == null) respawnPos = GameObject.Find("RespawnPos").transform;
+        player = (PlayerScripts) PlayerScripts.Create(respawnPos).AddTo(this);
+    }
+
+    public void GenerateNewLevel()
+    {
+        StartCoroutine(Co_GenerateNewLevel());
+    }
+
+    private IEnumerator Co_GenerateNewLevel()
+    {
+        Clear();
+        GameCanvas.Create().AddTo(this);
+        GameManager.Create().AddTo(this);
+        UIManager.Create().AddTo(this);
+        LevelManager.GetInstace.LoadLevel(LocalDataManager.currentLevel);
+        yield return null;
+        player = (PlayerScripts)PlayerScripts.Create(GameObject.Find("RespawnPos").transform).AddTo(this);
     }
 }
