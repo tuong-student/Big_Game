6d6b8a10ac5348731b72761ccea6b21ef37708e0
Mia <19110125@student.hcmute.edu.vn>
2023-01-04 03:10:26 +0700

    Add win/loose panel, add win sfx

--- Big_game/Assets/Scripts/UI/ResetUI.cs
+++ Big_game/Assets/Scripts/UI/ResetUI.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 public class ResetUI : MonoBehaviour
 {
     /*
-         public static float fireRate = 5;
-    public static float criticalRate = 5;
-    public static float speed = 5;
-    public static float damage = 1f;
+    public static int playerNumber = 1;
+    public static int gold = 100;
+    public static int currentLevel = 1;
+    public static float bonusDamage = 1f;
+    public static float bonusFireRate = 0;
+    public static float bonusReloadSpeed = 0;
+    public static float criticalRate = 0.5f;
+    public static float runSpeed = 5f;
+    public static float walkSpeed = 2f;
     public static float defence = 0f;
 
-    public static float health = 100;
-    public static float mana = 50;*/
+    public static float maxHealth = 100;
+    public static float mana = 50;
+    
+     */
 
     public void NewGame()
     {
-        LocalDataManager.bonusFireRate = 5;
-        LocalDataManager.criticalRate = 5;
-        LocalDataManager.runSpeed = 5;
+        LocalDataManager.playerNumber = 1;
+        LocalDataManager.gold = 100;
+        LocalDataManager.currentLevel = 1;
         LocalDataManager.bonusDamage = 1f;
+        LocalDataManager.bonusFireRate = 0;
+        LocalDataManager.bonusReloadSpeed = 0;
+        LocalDataManager.criticalRate = 0.5f;
+        LocalDataManager.runSpeed = 5f;
+        LocalDataManager.walkSpeed = 2f;
         LocalDataManager.defence = 0f;
         LocalDataManager.maxHealth = 100;
         LocalDataManager.mana = 50;
-        LocalDataManager.currentLevel = 1;
-        LocalDataManager.gold = 0;
+    }
 
-        //Main.GetInstance.GenerateNewLevel(); 
+    public void TryAgain()
+    {
+        LocalDataManager.gold = 100;
+        LocalDataManager.currentLevel = 1;
+        LocalDataManager.bonusDamage = 1f;
+        LocalDataManager.bonusFireRate = 0;
+        LocalDataManager.bonusReloadSpeed = 0;
+        LocalDataManager.criticalRate = 0.5f;
+        LocalDataManager.runSpeed = 5f;
+        LocalDataManager.walkSpeed = 2f;
+        LocalDataManager.defence = 0f;
+        LocalDataManager.maxHealth = 100;
+        LocalDataManager.mana = 50;
     }
 }
---
+++ Big_game/Assets/Scripts/UI/WinLooseUI.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinLooseUI : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup winPanel, loosePanel;
+    [SerializeField] private Button tryAgainButton, quitButton, playAgainButton, restartGameButton;
+    [SerializeField] private ResetUI reset;
+
+    private void Start()
+    {
+        WinPanelUnactivate();
+        LoosePanelUnactivate();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            WinPanelActivate();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LoosePanelActivate();
+        }
+    }
+
+    private void OnEnable()
+    {
+        tryAgainButton.onClick.AddListener(TryAgainFromLevel1);
+        quitButton.onClick.AddListener(ExitGame);
+        playAgainButton.onClick.AddListener(PlayAgainFromPickChar);
+        restartGameButton.onClick.AddListener(PlayAgainFromPickChar);
+    }
+
+    private void OnDisable()
+    {
+        tryAgainButton.onClick.RemoveListener(TryAgainFromLevel1);
+        quitButton.onClick.RemoveListener(ExitGame);
+        playAgainButton.onClick.RemoveListener(PlayAgainFromPickChar);
+        restartGameButton.onClick.RemoveListener(PlayAgainFromPickChar);
+    }
+
+
+    public void WinPanelActivate()
+    {
+        winPanel.alpha = 1;
+        winPanel.interactable = true;
+        winPanel.blocksRaycasts = true;
+    }
+    public void LoosePanelActivate()
+    {
+        loosePanel.alpha = 1;
+        loosePanel.interactable = true;
+        loosePanel.blocksRaycasts = true;
+
+    }
+    public void WinPanelUnactivate()
+    {
+        winPanel.alpha = 0;
+        winPanel.interactable = false;
+        winPanel.blocksRaycasts = false;
+
+    }
+    public void LoosePanelUnactivate()
+    {
+        loosePanel.alpha = 0;
+        loosePanel.interactable = false;
+        loosePanel.blocksRaycasts = false;
+
+    }
+
+    private void TryAgainFromLevel1()
+    {
+        LoosePanelUnactivate();
+        reset.TryAgain();
+    }
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+    private void PlayAgainFromPickChar()
+    {
+        if(loosePanel.alpha == 1)
+        {
+            LoosePanelUnactivate();
+        }
+        else
+        {
+            WinPanelUnactivate();
+        }
+        reset.NewGame();
+    }
+}
+
